c245dec1b89d92cca817daff528fc78d24bf8331
Giovanni De Cata s364170 <s312453@studenti.polito.it>
2026-06-05 17:23:41 +0200

    feature: implemented authentication features, solved several errors regarding SelectRandomStations

--- server/dao.js
+++ server/dao.js
@@ -3,7 +3,6 @@ import sqlite3 from "sqlite3";
 import { Link, Station, Dijkstra_finder,DUMP_LINK_LIST,DUMP_STATION_LIST } from "./Stations.js";
 import crypto from "crypto";
 const db = new sqlite3.Database("db.sqlite3",(err) => {if(err) throw err;});
-
 const select_all_stations = " SELECT NAME FROM STATIONS "
 const select_all_links = " SELECT * FROM LINKS"
 
@@ -33,39 +32,62 @@ async function SelectRandomStations(){
     const stations = await AllStations()
     const links = await AllLinks()
     let min_distance = 0
-    const start = null
-    const end = null;
+    let start = {}
+    let end = {};
     while(min_distance<3){
-        const randomIndex_1 = crypto.randomInt(0, stations.length)
-        const randomIndex_2 = crypto.randomInt(0, stations.length)
+        let randomIndex_1 = crypto.randomInt(0, stations.length)
+        let randomIndex_2 = crypto.randomInt(0, stations.length)
         let min_distance = Dijkstra_finder(stations,links,stations[randomIndex_1],stations[randomIndex_2])
         if(min_distance>=3){
             start = stations[randomIndex_1]
             end = stations[randomIndex_2]
+            return {
+        "from": start,
+        "to": end
         }
-        return [start,end]
+        
     }
+        
+    }
+    
 }
 
 async function GetStation(station){
     let res = await AllStations()
     return res.find((s)=>{
         return s.name === station.name;
-    })
+    }) || {}
 }
 
 async function GetLink(link){
     let res = await AllLinks()
     return res.find((l)=>{
         return l.from === link.from && l.to === link.to;
+    }) || {}
+}
+
+//user ops
+async function AuthUser(username,password){
+    return new Promise((resolve,reject)=>{
+        let sql = "SELECT * FROM USERS WHERE USERNAME = ?"
+        //i use sha256 for password encrypting for simplicity
+        const hash = crypto.createHash('sha256').update(password).digest('hex');
+        //sql injection, allows me to pass and switch ? character with thw values i want
+        db.get(sql,[username],(err,row)=>{
+            if(err) reject(err)
+            else if(!row) resolve(false)
+            else if(hash !== row.PASSWORD) resolve(false)
+            else resolve({username:row.USERNAME})
+        })
     })
 }
 
-
-//test operations
 let res = await AllStations()
-console.log(res)
+console.log(res[1])
 res = await AllLinks()
-console.log(res)
+console.log(res[1])
 console.log(await GetLink(DUMP_LINK_LIST[0]))
 console.log (await GetStation(DUMP_STATION_LIST[1]))
+console.log(await AuthUser("dummyuser","dummyuser12"))
+console.log(await SelectRandomStations())
+export {AuthUser,AllLinks,AllStations,GetLink,GetStation,SelectRandomStations}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,75 @@
 // imports
 import express from "express";
-
+import { Link,Station } from "./Stations.js";
+import { AllLinks, AllStations,GetLink,GetStation,AuthUser, SelectRandomStations, } from "./dao.js";
+import passport from "passport"
+import LocalStrategy from "passport-local"
+import session from "express-session";
 // init express
 const app = new express();
 const port = 3001;
+//middleware
+app.use(express.json());
+app.use(session({
+  secret: "Sto gia facendo un impresa!",
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(function verify(username,password,callback){
+  AuthUser(username,password).then((user)=>{
+    if(!user)return callback(null,false,{
+      message:"incorrect username and/or password"
+    });
+    return callback(null,user)
+  }).catch(err => callback(err))
+}))
+const isLoggedIn = (req, res, next) => {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  return res.status(401).json({error: "Not authorized"});
+}
 
+passport.serializeUser((user,callback)=>{
+  callback(null,{username:user.username})
+})
+passport.deserializeUser((user,callback)=>{
+  callback(null,user)
+})
 // activate the server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
+});
+
+app.get("/api/Stations",async (req,res)=>{
+  try {
+    let result
+    if(!req.query.station) result = await AllStations()
+    else result = await GetStation(new Station(req.query.station))
+    res.json(result)
+  } catch(err) {
+    res.status(500).json({error: err.message})
+  }
+})
+
+app.get("/api/Links",async (req,res)=>{
+  try {
+    let result
+    if(!(req.query.from && req.query.to)) result = await AllLinks()
+    else result = await GetLink(new Link(req.query.from,req.query.to))
+    res.json(result)
+  } catch(err) {
+    res.status(500).json({error: err.message})
+  }
+})
+
+app.get("/api/RandomStations",isLoggedIn, async (req,res)=>{
+  let result = await SelectRandomStations()
+  res.json(result)
+})
+//post methods
+app.post("/api/sessions", passport.authenticate("local"), function(req, res) {
+  return res.status(201).json(req.user);
 });
